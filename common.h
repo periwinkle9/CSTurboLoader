b4bce9c56f72aaf4c6e291e57cadf15cd1bbeea1
periwinkle <generic.username.625.125.25.5@gmail.com>
2022-01-21 17:01:23 -0500

    Change platform toolset Not necessary to target WinXP, I suppose

--- common.h
+++ common.h
@@ -1,5 +1,5 @@
 #pragma once
 
 #define WIN32_LEAN_AND_MEAN
-typedef struct IUnknown IUnknown; // Fix a compilation error with the particular Windows SDK I'm using
+//typedef struct IUnknown IUnknown; // Fix a compilation error with the particular Windows SDK I'm using
 #include <Windows.h>
